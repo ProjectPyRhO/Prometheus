f0ed61f42b32838c74b67f2844ec732acfa133d0
Ben Evans <ben.d.evans@gmail.com>
2016-08-22 19:05:41 +0100

    Correct nginx config file mounting path

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name docker-nginx -p 80:80 -v ~/html:/usr/share/nginx/html -v ./default.conf:/etc/nginx/conf.d/default.conf -d nginx
+docker run --name docker-nginx -p 80:80 -v ~/nginx/html:/usr/share/nginx/html -v ~/nginx/default.conf:/etc/nginx/conf.d/default.conf -d nginx
