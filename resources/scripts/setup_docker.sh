cde227bca6ca02a85dcabb2f25c51918bafe163e
Ben Evans <ben.d.evans@gmail.com>
2016-08-25 17:55:57 +0100

    Add linux-image-extra-virtual to installation

--- resources/scripts/setup_docker.sh
+++ resources/scripts/setup_docker.sh
@@ -8,7 +8,7 @@ sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58
 # sudo echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" >> /etc/apt/sources.list.d/docker.list
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee -a /etc/apt/sources.list.d/docker.list
 
-sudo apt-get install linux-image-extra-$(uname -r)
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
 sudo apt-get update
 sudo apt-get purge lxc-docker
 
